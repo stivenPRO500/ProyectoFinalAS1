b4fca5d3a1256f5be84591ff79c432b9c5daae06
kevinMarr22 <kmarroquing4@miumg.edu.gt>
2024-05-31 10:22:43 -0600

    Revert "PROYEC-10"
    This reverts commit a4a237d67dec1cf34d488028c3d26851312ce19f.

--- src/main/java/com/example/cinetrailer/controller/AdminController.java
+++ src/main/java/com/example/cinetrailer/controller/AdminController.java
@@ -35,68 +35,6 @@ public class  AdminController {
                 .addObject("peliculas", peliculas);
     }
 
-
-      @GetMapping("/peliculas/nuevo")
-    ModelAndView nuevaPelicula() {
-        List<Genero> generos = generoRepository.findAll(Sort.by("titulo"));
-        return new ModelAndView("admin/nueva-pelicula")
-                .addObject("pelicula", new Pelicula())
-                .addObject("generos", generos);
-    }
-    @PostMapping("/peliculas/nuevo")
-    ModelAndView crearPelicula(@Validated Pelicula pelicula, BindingResult bindingResult) {
-        if (bindingResult.hasErrors() || pelicula.getPortada().isEmpty()) {
-            if (pelicula.getPortada().isEmpty()) {
-                bindingResult.rejectValue("portada", "MultipartNotEmpty");
-            }
-            List<Genero> generos = generoRepository.findAll(Sort.by("titulo"));
-            return new ModelAndView("admin/nueva-pelicula")
-                    .addObject("pelicula", pelicula)
-                    .addObject("generos", generos);
-        }
-        String rutaPortada = fileSystemStorageService.storage(pelicula.getPortada());
-        pelicula.setRutaPortada(rutaPortada);
-        peliculaRepository.save(pelicula);
-        return new ModelAndView("redirect:/admin");
-    }
-
-
-
-    @GetMapping("/peliculas/{id}/editar")
-    ModelAndView editarPelicula(@PathVariable Integer id) {
-        Pelicula pelicula = peliculaRepository.getOne(id);
-        List<Genero> generos = generoRepository.findAll(Sort.by("titulo"));
-        return new ModelAndView("admin/editar-pelicula")
-                .addObject("pelicula", pelicula)
-                .addObject("generos", generos);
-    }
-
-
-    @PostMapping("/peliculas/{id}/editar")
-    ModelAndView actualizarPelicula(@PathVariable Integer id, @Validated Pelicula pelicula, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            List<Genero> generos = generoRepository.findAll(Sort.by("titulo"));
-            return new ModelAndView("admin/editar-pelicula")
-                    .addObject("pelicula", pelicula)
-                    .addObject("generos", generos);
-        }
-        Pelicula peliculaFromDb = peliculaRepository.getOne(id);
-        peliculaFromDb.setTitulo(pelicula.getTitulo());
-        peliculaFromDb.setSinopsis(pelicula.getSinopsis());
-        peliculaFromDb.setFechaEstreno(pelicula.getFechaEstreno());
-        peliculaFromDb.setYoutubeTrailerId(pelicula.getYoutubeTrailerId());
-        peliculaFromDb.setGeneros(pelicula.getGeneros());
-
-
-        if (!pelicula.getPortada().isEmpty()) {
-            fileSystemStorageService.delete(peliculaFromDb.getRutaPortada());
-            String rutaPortada = fileSystemStorageService.storage(pelicula.getPortada());
-            peliculaFromDb.setRutaPortada(rutaPortada);
-        }
-        peliculaRepository.save(peliculaFromDb);
-        return new ModelAndView("redirect:/admin");
-    }
-
     @PostMapping("/peliculas/{id}/eliminar")
     String eliminarPelicula(@PathVariable Integer id) {
         Pelicula pelicula = peliculaRepository.getOne(id);
